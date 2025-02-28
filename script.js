b08744b26c198beee2406fcf77309178cdfe7365
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-28 11:50:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(image) {
+	return new Promise((resolve,reject)=>{
+		const img= new Image();
+		img.src= image;
+		img.onload=()=>resolve(image);
+		img.onerror=()=>reject(`Failed to load image from ${image}`);
+	
+	});
+}
+
+function downloadImages(){
+	//show loading spinner
+	let prom =[];
+	images.forEach((image)=>{
+		prom.push(downloadImage(image.url));
+	});
+	
+	output.innerHTML = ``;
+
+	Promise.all(prom).then((values)=>{
+		document.getElementById("loading").style.display="none";
+		values.forEach((url)=>{
+			const img = document.createElement('img');
+	        img.src = url;
+			img.alt="not loaded"
+	        output.appendChild(img);	
+		});
+		
+		
+	}).catch((err)=>{
+		document.getElementById("loading").style.display="none";
+		let errDiv = document.getElementById("error");
+		errDiv.innerHTML =`<p>${err}<p>`;
+			
+	});
+
+}
+downloadImages();
+
