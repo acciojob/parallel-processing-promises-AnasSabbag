e970f6fbf063d8ce94c9309072dcf460bf4f8c14
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-03-02 10:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
